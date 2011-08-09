01115d353554938873b5650ee2b5c7f73c233960
pydanny <pydanny@gmail.com>
2011-08-08 20:47:52 -0700

    Pushing up djangozoom specific changes

--- settings.py
+++ settings.py
@@ -303,7 +303,7 @@ SUPPORTED_REPO = []
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
-    from local_settings import *
+    from djangozoom import *
 except ImportError:
     pass
 
